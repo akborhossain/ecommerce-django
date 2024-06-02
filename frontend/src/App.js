8777f1890032b0996667e08ce9ca6af5af0e01e7
Akbar Hossain <smakbor33@gmail.com>
2024-06-03 02:44:38 +0600

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from './pages/HomePage'
 function App() {
   return (
     <div>
       <Header />
-      <main  className="py-3">
+      <main className="py-3">
         <Container>
-          <h1>Welcome to Ecommerce </h1>
+          <HomePage/>
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,12 +6,14 @@ function Header() {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">Ecommerce</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/">Sign Up</Nav.Link>
+              <Nav.Link href="/">Login</Nav.Link>
+              <Nav.Link href="/"> <i className="fas fa-shopping-cart"> </i> Card</Nav.Link>
+
             </Nav>
           </Navbar.Collapse>
         </Container>
