44cfe624ae6f5e3582cee3a35779e4cde77a424a
Akbar Hossain <smakbor33@gmail.com>
2024-06-04 03:18:24 +0600

    product route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomePage from './pages/HomePage'
+import HomePage from "./pages/HomePage";
+import ProductPage from './pages/ProductPage'
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage/>
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+            <Route path="/product/:id" element={<ProductPage/>}/>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Ecommerce</Navbar.Brand>
+          <LinkContainer to="/">
+              <Navbar.Brand >Ecommerce</Navbar.Brand>
+
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Sign Up</Nav.Link>
-              <Nav.Link href="/">Login</Nav.Link>
-              <Nav.Link href="/"> <i className="fas fa-shopping-cart"> </i> Card</Nav.Link>
-
+            <LinkContainer to="/signup">
+                <Nav.Link >Sign Up</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+                <Nav.Link >Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/card">
+                <Nav.Link > <i className="fas fa-shopping-cart"> </i> Card</Nav.Link>
+            </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
