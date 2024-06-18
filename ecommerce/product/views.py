6cd9b80a66636b1a56a3c1bab26611c81d4640b7
Akbar Hossain <smakbor33@gmail.com>
2024-06-18 21:45:15 +0600

    model

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -3,14 +3,17 @@ from .products import products
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from .models import *
+from .serializers import *
 # Create your views here.
 
 class ProductView(APIView):
     def get(self, request, pk=None):
         if pk is not None:
-            for i in products:
-                if i['_id']== pk:
-                    product=i
-            return Response(product)
-        return Response(products)
+            product=Product.objects.get(_id=pk)
+            serializerData=ProductSerializer(product, many=False)
+            return Response(serializerData.data)
+        products=Product.objects.all()
+        serializerData=ProductSerializer(products, many=True)
+
+        return Response(serializerData.data)
