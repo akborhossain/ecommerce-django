54e578121e66d14b0e12a2b0dcfa777773833331
Md. Akbar Hossain <smakbor33@gmail.com>
2024-12-25 21:13:59 +0600

    merge

--- ecommerce/product/urls.py
+++ ecommerce/product/urls.py
@@ -3,12 +3,9 @@ from django import views
 from .views import *
 
 urlpatterns=[
-<<<<<<< HEAD
     path('users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-=======
     path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/profile/', UserProfileView.as_view()),
->>>>>>> d83d40805d2cc0daeb97ebe0a2bcc8245a688ba4
     path('products/', ProductView.as_view()),
     path('products/<str:pk>/', ProductView.as_view()),
 
