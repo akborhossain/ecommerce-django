ee253cb41149f64b6e86ab3f0b0c1a7051ef7a78
Md. Akbar Hossain <smakbor33@gmail.com>
2024-12-31 08:55:22 +0600

    changes

--- ecommerce/product/serializers.py
+++ ecommerce/product/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
+    name= serializers.SerializerMethodField(read_only=True)
     class Meta:
         model= User
-        fields=['id', 'username', 'email']
+        fields=['id', 'username', 'email', 'name', 'is_staff']
+
+    def get_name(self,obj):
+        name = obj.first_name +" "+ obj.last_name
+        if name==" ":
+            name= obj.username
+        return name
+    
+class UserSerializerWithToken(UserSerializer):
+    token=serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model=User
+        fields=['id', 'username', 'email', 'name', 'is_staff', 'token']
+    def get_token(self,obj):
+        token=RefreshToken.for_user(obj)
+        return str(token)
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
--- ecommerce/product/urls.py
+++ ecommerce/product/urls.py
@@ -4,8 +4,9 @@ from .views import *
 
 urlpatterns=[
     path('users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', RegisterUser.as_view(), name='register'),
     path('users/profile/', UserProfileView.as_view()),
+    path('users/',UserView.as_view()),
     path('products/', ProductView.as_view()),
     path('products/<str:pk>/', ProductView.as_view()),
 
