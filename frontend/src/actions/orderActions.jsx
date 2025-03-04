45cf9eccd8607359e3ca07a5dfa1e8cdea594b9f
Md. Akbar Hossain <smakbor33@gmail.com>
2025-03-04 17:30:46 +0600

    get order

--- frontend/src/actions/orderActions.jsx
+++ frontend/src/actions/orderActions.jsx
@@ -2,7 +2,10 @@ import axios from 'axios';
 import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
-    ORDER_CREATE_FAIL
+    ORDER_CREATE_FAIL,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL
 } from '../constants/orderConstants'
 
 import { CART_CLEAR_ITEM } from '../constants/cartConstants'
@@ -45,6 +48,48 @@ export const createOrder = (order) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: ORDER_CREATE_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        });
+
+    }
+}
+
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_DETAILS_REQUEST
+        })
+        const { userLogin: { userInfo }, } = getState()
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const { data, status } = await axios.get(
+            `orders/${id}`,
+            config
+        )
+        if (status === 200) {
+            dispatch({
+                type: ORDER_DETAILS_SUCCESS,
+                payload: data.data,
+            });
+
+        }
+        else {
+            dispatch({
+                type: ORDER_DETAILS_FAIL,
+                payload: data.detail || 'Order failed.',
+            });
+        }
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_DETAILS_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message,
